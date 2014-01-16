aa3673a87d305e4f494580004390bd86b8f19f2b
Crystal McDonald <stalmcdonald@fullsail.edu>
2014-01-16 12:58:02 -0800

    Superhero Theme -complete (working)
    Button now plays theme song and pauses.

--- src/com/cm/uberhero/ThemeSongActivity.java
+++ src/com/cm/uberhero/ThemeSongActivity.java
@@ -1,3 +1,8 @@
+/*
+ * Theme Song Activity
+ * Plays popular superhero themes
+ * User can use them as their own theme music when performing activities
+ */
 package com.cm.uberhero;
 
 import android.app.Activity;
@@ -8,44 +13,123 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class ThemeSongActivity extends Activity implements OnClickListener{
+public class ThemeSongActivity extends Activity {
 	
-	//Button supermanbttn, batmanbttn, jlbttn, JLAmbttn;
 	Button supermanTheme, batmanTheme, jlTheme, JLATheme;
-	String supermanStart, batmanStart, jlStart, jlaStart;
-	MediaPlayer mediaPlay;
+	String supermanStart, batStart, jlStart, jluStart;
+	MediaPlayer mediaPlay, batMediaPlay, jlMediaPlay, jluMediaPlay;
 	Context _context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.theme);
-		
+	
 		//set context
 		_context = this;
 		
-				
+		/*
+		 * Superman Button
+		 */
+		mediaPlay = MediaPlayer.create( _context, R.raw.supermantheme);
 		//Set buttons
 		supermanTheme = (Button) findViewById(R.id.supermanbttn);
 		supermanStart = "Play Superman Theme";
+		//start button 1 listener
+		supermanTheme.setOnClickListener(new OnClickListener(){
+					@Override
+					public void onClick(View v){
+						//starting and stopping the audio
+						if(supermanStart.equals("Play Superman Theme")){
+							mediaPlay.start();
+							supermanStart = "Pause";
+							supermanTheme.setText(supermanStart);
+						}else {
+							mediaPlay.pause();
+							supermanStart = "Play Superman Theme";//changes text back to play
+							supermanTheme.setText(supermanStart);					
+						}
+					}
+					
+				});
 		
+		/*
+		 * Batman Button
+		 */
+		batMediaPlay = MediaPlayer.create( _context, R.raw.battheme);
+		//Set button
 		batmanTheme = (Button) findViewById(R.id.batmanbttn);
-		batmanStart = "Play Batman Theme";
+		batStart = "Play Batman Theme";
+		//start button 2 listener
+		batmanTheme.setOnClickListener(new OnClickListener(){
+					@Override
+					public void onClick(View v1){
+
+						//starting and stopping the audio
+						if(batStart.equals("Play Batman Theme")){
+							batMediaPlay.start();
+							batStart = "Pause";
+							batmanTheme.setText(batStart);
+						}else {
+							batMediaPlay.pause();
+							batStart = "Play Batman Theme";//changes text back to play
+							batmanTheme.setText(batStart);					
+						}
+					}	
+				});
 		
+		/*
+		 * Justice League Button
+		 */
+		jlMediaPlay = MediaPlayer.create( _context, R.raw.jltheme);
+		//Set button
 		jlTheme = (Button) findViewById(R.id.jlbttn);
 		jlStart = "Play Justice League Theme";
+		//start button 3 listener
+		jlTheme.setOnClickListener(new OnClickListener(){
+					@Override
+					public void onClick(View v2){
+
+						//starting and stopping the audio
+						if(jlStart.equals("Play Justice League Theme")){
+							jlMediaPlay.start();
+							jlStart = "Pause";
+							jlTheme.setText(jlStart);
+						}else {
+							jlMediaPlay.pause();
+							jlStart = "Play Justice League Theme";//changes text back to play
+							jlTheme.setText(jlStart);					
+						}
+					}	
+				});
 		
+		
+		/*
+		 * Justice League Unlimited Button
+		 */
+		jluMediaPlay = MediaPlayer.create( _context, R.raw.jlutheme);
+		//Set button
 		JLATheme = (Button) findViewById(R.id.JLAmbttn);
-		jlaStart="Play Justice League Unlimited Theme";
+		jluStart = "Play Justice League Unlimited Theme";
+		//start button 1 listener
+		JLATheme.setOnClickListener(new OnClickListener(){
+					@Override
+					public void onClick(View v3){
+
+						//starting and stopping the audio
+						if(jluStart.equals("Play Justice League Unlimited Theme")){
+							jluMediaPlay.start();
+							jluStart = "Pause";
+							JLATheme.setText(jluStart);
+						}else {
+							jluMediaPlay.pause();
+							jluStart = "Play Justice League Unlimited Theme";//changes text back to play
+							JLATheme.setText(jlStart);					
+						}
+					}	
+				});
 		
-		//set up onclick listeners for buttons
-		findViewById(R.id.batmanbttn).setOnClickListener(this);
-		findViewById(R.id.supermanbttn).setOnClickListener(this);
-		findViewById(R.id.jlbttn).setOnClickListener(this);
-		findViewById(R.id.JLAmbttn).setOnClickListener(this);
 	}
-	
-	 
 
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onDestroy()
@@ -55,6 +139,9 @@ public class ThemeSongActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		mediaPlay.release();
+		batMediaPlay.release();
+		jlMediaPlay.release();
+		jluMediaPlay.release();
 	}
 
 
@@ -67,6 +154,9 @@ public class ThemeSongActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		super.onPause();
 		mediaPlay.pause();
+		batMediaPlay.pause();
+		jlMediaPlay.pause();
+		jluMediaPlay.pause();
 	}
 
 
@@ -79,105 +169,8 @@ public class ThemeSongActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		super.onStop();
 		mediaPlay.stop();
-	}
-
-
-
-	@Override
-	public void onClick(View v) {
-		// setup switch case buttons for music
-		switch (v.getId()){
-		
-		case R.id.supermanbttn:
-//			mediaPlay = MediaPlayer.create( _context, R.raw.supermantheme);
-//			try{
-//				if(mediaPlay != null){
-//					if (mediaPlay.isPlaying()){
-//					
-//					mediaPlay.stop();
-//					}else{
-//						
-//						mediaPlay.start();
-//					}
-//				}
-//			}catch (Exception e){
-//				Log.e("play_pause",""+e);
-//				
-//			}
-			//add music/sound to media player
-			mediaPlay = MediaPlayer.create( _context, R.raw.supermantheme);
-			//starting and stopping the audio
-			if(supermanStart.equals("Play Superman Theme")){
-				mediaPlay.start();
-				supermanStart = "Pause";
-				supermanTheme.setText(supermanStart);
-			}	
-			else{
-				if(supermanStart.equals("Pause")){
-					mediaPlay.stop();
-				supermanStart = "Play Superman Theme";//changes text back to play theme
-				supermanTheme.setText(supermanStart);	
-			}
-			}
-			break;
-			
-		case R.id.batmanbttn:
-			//add music/sound to media player
-			mediaPlay = MediaPlayer.create( _context, R.raw.battheme);
-			//starting and stopping the audio
-			if(batmanStart.equals("Play Batman Theme")){
-				mediaPlay.start();
-				batmanStart = "Pause";
-				batmanTheme.setText(batmanStart);
-				
-			}else {
-				mediaPlay.pause();
-				batmanStart = "Play Batman Theme";//changes text back to play theme
-				batmanTheme.setText(batmanStart);					
-			}
-			break;
-			
-		case R.id.jlbttn:
-			//add music/sound to media player
-			mediaPlay = MediaPlayer.create( _context, R.raw.jltheme);
-			//starting and stopping the audio
-			if(jlStart.equals("Play Justice League Theme")){
-				mediaPlay.start();
-				jlStart = "Pause";
-				jlTheme.setText(jlStart);
-				
-			}else {
-				mediaPlay.pause();
-				jlStart = "Play Justice League Theme";//changes text back to play theme
-				jlTheme.setText(jlStart);					
-			}
-			break;
-			
-		case R.id.JLAmbttn:
-			//add music/sound to media player
-			mediaPlay = MediaPlayer.create( _context, R.raw.jlutheme);
-			//starting and stopping the audio
-			if(jlaStart.equals("Play Justice League Unlimited Theme")){
-				mediaPlay.start();
-				jlaStart = "Pause";
-				JLATheme.setText(jlaStart);
-				
-			}else {
-				mediaPlay.pause();
-				jlaStart = "Play Justice League Unlimited Theme";//changes text back to play theme
-				JLATheme.setText(jlaStart);	
-			
-			}
-			break;
-		
-		
-		}	
-	}
-	
-	public void onClick1(View v1) {
-		if(mediaPlay.equals("Pause")){
-			mediaPlay.stop();
-	
-	}
+		batMediaPlay.stop();
+		jlMediaPlay.stop();
+		jluMediaPlay.stop();
 	}
 }
